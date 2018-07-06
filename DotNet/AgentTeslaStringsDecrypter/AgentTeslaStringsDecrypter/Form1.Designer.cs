c74fd653a93b0c8ae44d3f1fb8a421e8922b4a68
futex <remchp@gmail.com>
2018-07-06 14:44:55 +0200

    add Agent Tesla

---
+++ DotNet/AgentTeslaStringsDecrypter/AgentTeslaStringsDecrypter/Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace AgentTeslaStringsDecrypter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button2 = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(62, 70);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(116, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Decrypt strings";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(64, 23);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(116, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Browse for assembly";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(244, 114);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Agent Tesla Strings Decrypter";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+    }
+}
+
---
+++ DotNet/AgentTeslaStringsDecrypter/AgentTeslaStringsDecrypter/Form1.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using dnlib.DotNet;
+using dnlib.DotNet.MD;
+using dnlib.DotNet.Emit;
+using System.Security.Cryptography;
+using System.IO;
+
+namespace AgentTeslaStringsDecrypter
+{
+    public partial class Form1 : Form
+    {
+        string inputAssemblyName;
+
+        public static string DecryptString(string str)
+        {
+            try
+            {
+                string strPassword = "amp4Z0wpKzJ5Cg0GDT5sJD0sMw0IDAsaGQ1Afik6NwXr6rrSEQE=";
+                string s = "aGQ1Afik6NampDT5sJEQE4Z0wpsMw0IDAD06rrSswXrKzJ5Cg0G=";
+                string strHashName = "SHA1";
+                int iterations = 2;
+                int num = 256;
+                string s2 = "@1B2c3D4e5F6g7H8";
+                byte[] bytes = Encoding.ASCII.GetBytes(s2);
+                byte[] bytes2 = Encoding.ASCII.GetBytes(s);
+                byte[] array = Convert.FromBase64String(str);
+                PasswordDeriveBytes passwordDeriveBytes = new PasswordDeriveBytes(strPassword, bytes2, strHashName, iterations);
+                byte[] bytes3 = passwordDeriveBytes.GetBytes(num / 8);
+                ICryptoTransform transform = new RijndaelManaged
+                {
+                    Mode = CipherMode.CBC
+                }.CreateDecryptor(bytes3, bytes);
+                MemoryStream memoryStream = new MemoryStream(array);
+                CryptoStream cryptoStream = new CryptoStream(memoryStream, transform, CryptoStreamMode.Read);
+                byte[] array2 = new byte[array.Length];
+                int count = cryptoStream.Read(array2, 0, array2.Length);
+                memoryStream.Close();
+                cryptoStream.Close();
+                return Encoding.UTF8.GetString(array2, 0, count);
+            }
+            catch (Exception ex)
+            {
+               // if (ex.Source != null)
+                // MessageBox.Show("DecryptString IOException source: {0}", ex.Message);
+
+                return "null";
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public static void DecryptStringsDnlib(String executable)
+        {
+            try
+            {
+                int count = 0;
+                int countMethod = 0;
+                int countModule = 0;
+
+                string outputFolderName = Path.GetDirectoryName(executable);
+                string filename = Path.GetFileNameWithoutExtension(executable);
+
+                ModuleDefMD module = ModuleDefMD.Load(executable);
+
+                foreach (TypeDef type in module.GetTypes())
+                {
+                    countModule++;
+
+                    foreach (MethodDef method in type.Methods)
+                    {
+                        //if (!method.HasBody)
+                        //   break;
+                        if (!method.HasBody || method.IsConstructor)
+                            continue;
+
+                        countMethod++;
+
+                        for (int i = 0; i < method.Body.Instructions.Count; i++)
+                        {
+                            if (method.Body.Instructions[i].OpCode.Value == 114) //OpCodes.Ldstr)
+                            {
+                                if (method.Body.Instructions[i + 1].OpCode == OpCodes.Call)
+                                {
+                                    var cryptedstring = method.Body.Instructions[i].Operand.ToString();
+
+                                    string decryptedstring = DecryptString(cryptedstring);
+
+                                    //For exception max stack value
+                                    method.Body.KeepOldMaxStack = true;
+                                    method.Body.Instructions[i].OpCode = OpCodes.Ldstr;
+                                    method.Body.Instructions[i].Operand = decryptedstring;
+
+                                    method.Body.Instructions.Remove(method.Body.Instructions[i + 1]);
+
+                                    count++;
+                                }
+                            }
+                        }
+                    }
+                }
+
+               //MessageBox.Show("Total: " + count + " strings found." + " Number of methods: " + countMethod + " number of modules: " + countModule);
+
+               String outputfilename = outputFolderName +" \\" + filename + "_uncrypt.exe";
+
+               module.Write(outputfilename);
+
+               MessageBox.Show("Clean sample write in: " + outputfilename + "\n" + count + " strings decrypted.");
+
+            }
+            catch (Exception ex)
+            {
+                if (ex.Source != null)
+                    MessageBox.Show("DecryptStringsDnlib2 IOException source: {0}", ex.Message);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+    
+            if (openFileDialog1.ShowDialog() == DialogResult.OK) 
+            {
+                inputAssemblyName = openFileDialog1.FileName;
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (inputAssemblyName == null)
+                MessageBox.Show("Select an assembly");
+            else
+                DecryptStringsDnlib(inputAssemblyName);
+        }
+
+
+    }
+}
