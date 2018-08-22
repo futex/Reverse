78616bb66420b89865881d1f0831b397c6d15ad6
futex <remchp@gmail.com>
2018-08-22 17:41:59 +0200

    update ircbot

--- IDA/Scripts/ircbot.py
+++ IDA/Scripts/ircbot.py
@@ -1,5 +1,5 @@
 #For ircbot malware sample: d9b2e9d8826cab35e3287a0a35248f40
-#PS: Exec the sample after the xor key creation
+#PS: Exec the sample for appcall execution
 
 from idaapi import *
 import struct
