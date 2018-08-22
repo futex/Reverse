787305636879cf2ba8755a7a0063d465e5562e9e
futex <remchp@gmail.com>
2018-08-22 17:40:45 +0200

    update ircbot

--- IDA/Scripts/ircbot.py
+++ IDA/Scripts/ircbot.py
@@ -1,6 +1,29 @@
 #For ircbot malware sample: d9b2e9d8826cab35e3287a0a35248f40
 #PS: Exe the sample after the xor key creation
 
+from idaapi import *
+import struct
+
+def convert_little_endian(value):
+	return struct.unpack('>I',struct.pack('<I', value))[0]
+
+def patch_bytes(address, bytes):
+
+	tmp_value_value=struct.unpack('>L', bytes)[0]
+	new_value=convert_little_endian(tmp_value_value)
+
+	PatchDword(address, new_value)
+
+def patch():
+
+	key = bytearray([0xE0, 0xB2, 0xAA, 0xD9, 0xC0, 0xE5, 0xA4, 0xF9, 0xB5, 0xA6, 0x9C, 0x97, 0xB0, 0xD0, 0x8C, 0x8D])
+	address= 0x0040EC10
+
+	patch_bytes(address, key[0:4])
+	patch_bytes(address+4, key[4:8])
+	patch_bytes(address+8, key[8:12])
+	patch_bytes(address+12, key[12:16])
+
 def decrypt(func):
 	datastart = SegByBase(SegByName(".data"))
 	dataend = SegEnd(datastart)
@@ -23,6 +46,8 @@ idaapi.process_ui_action("msglist:Clear")
 
 FUNC_NAME = "XORStringDecrypt" #0x0040216
 PROTO = "int __cdecl XORStringDecrypt(char *str);".format(FUNC_NAME)
+
+patch()
 
 decrypt_function = Appcall.proto(FUNC_NAME, PROTO)
 
